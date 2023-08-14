83f640de9f42e2e3fcf916662a9b501a35aa752f
Colson <colsoncalifornication@gmail.com>
2023-08-14 07:37:31 +0545

    load and parse the csv file

--- build/index.js
+++ build/index.js
@@ -1,2 +1,19 @@
 "use strict";
-console.log('footwin');
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const fs_1 = __importDefault(require("fs"));
+const matches = fs_1.default
+    .readFileSync('football.csv', {
+    // readFileSync can read any type of file: image, executable, json, csv, ..
+    // Encoding tells readFileSync that we expect text content encoded
+    // with utf-8 and that will return us back string representing the content
+    // If we don't add encoding flag, it would give a buffer with raw data
+    encoding: 'utf-8',
+})
+    .split('\n')
+    .map((row) => {
+    return row.split(',');
+});
+console.log(matches);
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,16 @@
-console.log('footwin')
+import fs from 'fs';
+
+const matches = fs
+  .readFileSync('football.csv', {
+    // readFileSync can read any type of file: image, executable, json, csv, ..
+    // Encoding tells readFileSync that we expect text content encoded
+    // with utf-8 and that will return us back string representing the content
+    // If we don't add encoding flag, it would give a buffer with raw data
+    encoding: 'utf-8',
+  })
+  .split('\n')
+  .map((row: string): string[] => {
+    return row.split(',');
+  });
+
+console.log(matches);
